c9c9b85375925283b0329469cb8db90d10665d3a
DRG2Boss <113391397+DRG2Boss@users.noreply.github.com>
2022-10-21 10:29:43 -0400

    Cleanup and assignment details .png

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+# Isaiah Marshall | Everything completed | "It just works.." ..as long as your Arcade library is installed.
+
+# First, we need to import the installed library "Arcade."
 import arcade
 
-# First, lets open and read the file.
+# Now lets open and read the file.
 file1 = open("nationsPop.txt", "r")
 
 # This will create a list where each line is an element within said list.
@@ -22,38 +25,38 @@ arcade.draw_text("Populations of The Largest Nations On Earth", 675, 875, arcade
 xScale = (1600 - 75) / len(range(100, 1500, 100))
 
 # This variable works in conjunction with xScale to properly space our labels and bars.
-currentX = 75
+beginningX = 75
 
 # Next we need to run through a pretty complicated 'for' loop in order to create a few objects.
 for line in allLines:
 
     # The first two lines create our x-axis labels.
     splitNationData = line.split(',')
-    currentLabel = arcade.Text(splitNationData[0], currentX, 50, arcade.color.BLACK)
+    currentLabel = arcade.Text(splitNationData[0], beginningX, 50, arcade.color.BLACK)
 
-    # Similar to xScale, this is used to properly scale our bars within the graph.
+    # Similar to xScale, this will be used to help properly scale our bars within the graph.
     barScale = (int(splitNationData[1])-100000000)/1000000
 
     # Now we want to create the bars that exist within the graph.
     # An ifElse is used to properly define the colors of the graph based on whether our rate of change is pos or neg.
     if float(splitNationData[2]) < 0:
-        arcade.draw_xywh_rectangle_filled(currentX, 75, 100, barScale, arcade.color.RED)
+        arcade.draw_xywh_rectangle_filled(beginningX, 75, 100, barScale, arcade.color.RED)
     else:
-        arcade.draw_xywh_rectangle_filled(currentX, 75, 100, barScale, arcade.color.GREEN)
-    # Using both of the earlier variables, I'm able to get the spacing correct between all of my labels and graphs.
-    # "x += y" is the same as "x = x+y"
-    currentX += xScale
+        arcade.draw_xywh_rectangle_filled(beginningX, 75, 100, barScale, arcade.color.GREEN)
+    # Using both of the earlier variables, I'm able to get the spacing correct between all of my X labels and graphs.
+    # Note: "x += y" is the same as "x = x+y"
+    beginningX += xScale
     currentLabel.draw()
 
 # Now we need to label our Y-Axis using a much similar flow. We'll start with declaring similar global variables.
 yScale = (900 - 75) / len(range(100, 1500, 100))
-currentY = 75
+beginningY = 75
 
 for number in range(100, 1500, 100):
 
     # The following two lines will draw our y-axis labels and then properly space them using the same logic as above.
-    currentLabel = arcade.Text(f"{number}M", 5, currentY, arcade.color.BLACK)
-    currentY += yScale
+    currentLabel = arcade.Text(f"{number}M", 5, beginningY, arcade.color.BLACK)
+    beginningY += yScale
     currentLabel.draw()
 
 arcade.finish_render()
